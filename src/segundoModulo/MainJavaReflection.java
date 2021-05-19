a79d29fdc6c5e3339257f908e17f4824bc4bc85a
danieleperse <danieleperse3@gmail.com>
2021-05-19 20:45:18 -0300

    ultimo exemplo aula java reflections

--- src/segundoModulo/MainJavaReflection.java
+++ src/segundoModulo/MainJavaReflection.java
@@ -1,5 +1,7 @@
 package segundoModulo;
 
+import java.lang.reflect.Field;
+
 import segundoModulo.alunos.ValidationException;
 import segundoModulo.credito.AnalisadorCredito;
 import segundoModulo.credito.externo.Pessoa;
@@ -8,15 +10,29 @@ public class MainJavaReflection {
 
 	public static void main(String[] args) throws ValidationException {
 		
-		Pessoa pessoa = getPessoa();
-		AnalisadorCredito analisador = new AnalisadorCredito();
-		boolean resultado = analisador.analisarCredito(pessoa);
+//		Pessoa pessoa = getPessoa();
+//		AnalisadorCredito analisador = new AnalisadorCredito();
+//		boolean resultado = analisador.analisarCredito(pessoa);
+//		
+//		if (resultado) {
+//			System.out.println("credito Aprovado");
+//		} else {
+//			System.out.println("Credito Reprovado");
+//		}
 		
-		if (resultado) {
-			System.out.println("credito Aprovado");
-		} else {
-			System.out.println("Credito Reprovado");
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("Pessoa Teste");
+		// com o java reflextions, conseguimos alterar em tempo real, até mesmo atributos de origem privada (mas não é aconselhável, isso foi apenas um teste)
+		try {
+			Field nome = Pessoa.class.getDeclaredField("nome");
+			nome.setAccessible(true);
+			nome.set(pessoa, "Mudei o valor");
+		} catch(NoSuchFieldException e) {
+			System.out.println("Atributo não encontrado!");
+		} catch (IllegalAccessException e) {
+			System.out.println("Não tem acesso!");
 		}
+		System.out.print(pessoa.getNome());	
 	}
 	
 	public static Pessoa getPessoa() {
